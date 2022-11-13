3404ffd800d4d9e2689507532debcd92b38adf66
vbasakov <vbasakov@gmail.com>
2022-11-13 11:02:39 +0300

    spring starter

---
+++ src/test/kotlin/ru/vbasakov/church/scheduler/SchedulerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vbasakov.church.scheduler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
