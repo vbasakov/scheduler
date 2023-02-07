b8ff24f1d0b265f056b5f85208034709bff36c92
Vadim Basakov <basakov.v.yu@sberbank.ru>
2023-02-07 16:34:48 +0300

    json 2

--- src/main/kotlin/ru/vbasakov/church/scheduler/service/JsonParser.kt
+++ src/main/kotlin/ru/vbasakov/church/scheduler/service/JsonParser.kt
@@ -2,9 +2,20 @@ package ru.vbasakov.church.scheduler.service
 
 import org.springframework.stereotype.Service
 
+val apiUrl = "https://sheets.googleapis.com/v4/spreadsheets"
+
+val apiKey = "AIzaSyDmXe7UMHKblpgf2MgtzV6jjfPHXsuy8Lk"
+
+val documentId = "1hvVfxdsuVKjwJF4YmB4LxYSBZ_wAP9rwqUVQbTYHEOg"
+
+val dropdownList = "выпадающий%20список"
+val dataInput = "Внесение%20данных%20в%20график"
+
 @Service
 class JsonParser {
 
-    val getAllJson = "https://sheets.googleapis.com/v4/spreadsheets/1hvVfxdsuVKjwJF4YmB4LxYSBZ_wAP9rwqUVQbTYHEOg/?key=AIzaSyDmXe7UMHKblpgf2MgtzV6jjfPHXsuy8Lk"
-    val getSheetJson = "https://sheets.googleapis.com/v4/spreadsheets/1hvVfxdsuVKjwJF4YmB4LxYSBZ_wAP9rwqUVQbTYHEOg/?key=AIzaSyDmXe7UMHKblpgf2MgtzV6jjfPHXsuy8Lk"
+    val getAllJson  = "$apiUrl/$documentId/?key=$apiKey"
+
+    val getDropdownListJson = "$apiUrl/$documentId/values/$dropdownList?key=$apiKey"
+    val getDataInputListJson = "$apiUrl/$documentId/values/$dataInput?key=$apiKey"
 }
